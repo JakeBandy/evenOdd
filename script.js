24614163bb0a8dad245e3703781293a4772b8dae
JakeBandy <jakebandy13@hotmail.com>
2022-03-01 21:04:21 -0600

    Started

---
+++ script.js
@@ -0,0 +1,9 @@
+let usersWord = ""
+
+    function updateWord(element) {
+      usersWord = element.value
+
+      console.log(usersWord)
+    }
+    let usersWord = ""
+    let oddList = document.getElementById("odd")
